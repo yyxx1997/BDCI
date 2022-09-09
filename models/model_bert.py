180202ce733f4aca092f113efd0b34ee8d7af2bc
yanxu73 <yyxx20150909@163.com>
2022-09-09 19:28:43 +0800

    remove assoftmax & add focal loss

--- models/model_bert.py
+++ models/model_bert.py
@@ -1,14 +1,14 @@
 from transformers import BertModel
 from torch import nn
 import torch.nn.functional as F
-from models.tricks import AS_Softmax, compute_kl_loss, FocalLoss
+from models.tricks import AS_Softmax, compute_kl_loss, MultiCEFocalLoss
 
 class BaselineBert(nn.Module):
     def __init__(self,config,text_encoder):
         super().__init__()
         self.config = config
         self.text_encoder = BertModel.from_pretrained(text_encoder, add_pooling_layer=False)
-        self.as_softmax = self.config['as_softmax'] if 'as_softmax' in self.config else None
+        self.focal_loss = MultiCEFocalLoss(class_num=config['category'])
         self.textual_cls_head = nn.Sequential(
             nn.Linear(self.text_encoder.config.hidden_size,
                       self.text_encoder.config.hidden_size),
@@ -24,7 +24,6 @@ class BaselineBert(nn.Module):
         prediction = self.textual_cls_head(te_hiddens)
         output['prediction'] = prediction
         if train:
-            prediction, targets = AS_Softmax(self.as_softmax)(prediction, targets)
-            loss = F.cross_entropy(prediction, targets, ignore_index=-1)
+            loss = self.focal_loss(prediction, targets)
             output['loss'] = loss
         return output
--- models/tricks.py
+++ models/tricks.py
@@ -61,67 +61,30 @@ def compute_kl_loss(p, q, pad_mask=None):
     loss = (p_loss + q_loss) / 2
     return loss
 
-class FocalLoss(nn.Module):
-    r"""
-        This criterion is a implemenation of Focal Loss, which is proposed in 
-        Focal Loss for Dense Object Detection.
 
-            Loss(x, class) = - \alpha (1-softmax(x)[class])^gamma \log(softmax(x)[class])
-
-        The losses are averaged across observations for each minibatch.
-
-        Args:
-            alpha(1D Tensor, Variable) : the scalar factor for this criterion
-            gamma(float, double) : gamma > 0; reduces the relative loss for well-classiﬁed examples (p > .5), 
-                                   putting more focus on hard, misclassiﬁed examples
-            size_average(bool): By default, the losses are averaged over observations for each minibatch.
-                                However, if the field size_average is set to False, the losses are
-                                instead summed for each minibatch.
-
-
-    """
-    def __init__(self, class_num, alpha=None, gamma=2, size_average=True):
-        super(FocalLoss, self).__init__()
+class MultiCEFocalLoss(torch.nn.Module):
+    def __init__(self, class_num, gamma=2, alpha=None, reduction='mean'):
+        super(MultiCEFocalLoss, self).__init__()
         if alpha is None:
             self.alpha = Variable(torch.ones(class_num, 1))
         else:
-            if isinstance(alpha, Variable):
-                self.alpha = alpha
-            else:
-                self.alpha = Variable(alpha)
+            self.alpha = alpha
         self.gamma = gamma
-        self.class_num = class_num
-        self.size_average = size_average
+        self.reduction = reduction
+        self.class_num =  class_num
 
-    def forward(self, inputs, targets):
-        N = inputs.size(0)
-        C = inputs.size(1)
-        P = F.softmax(inputs)
-
-        class_mask = inputs.data.new(N, C).fill_(0)
-        class_mask = Variable(class_mask)
-        ids = targets.view(-1, 1)
-        class_mask.scatter_(1, ids.data, 1.)
-        #print(class_mask)
-
-
-        if inputs.is_cuda and not self.alpha.is_cuda:
-            self.alpha = self.alpha.cuda()
-        alpha = self.alpha[ids.data.view(-1)]
-
-        probs = (P*class_mask).sum(1).view(-1,1)
-
+    def forward(self, predict, target):
+        pt = F.softmax(predict, dim=1) # softmmax获取预测概率
+        class_mask = F.one_hot(target, self.class_num) #获取target的one hot编码
+        ids = target.view(-1, 1) 
+        alpha = self.alpha[ids.data.view(-1)].to(predict.device) # 注意，这里的alpha是给定的一个list(tensor),里面的元素分别是每一个类的权重因子
+        probs = (pt * class_mask).sum(1).view(-1, 1) # 利用onehot作为mask，提取对应的pt
         log_p = probs.log()
-        #print('probs size= {}'.format(probs.size()))
-        #print(probs)
+        # 同样，原始ce上增加一个动态权重衰减因子
+        loss = -alpha * (torch.pow((1 - probs), self.gamma)) * log_p
 
-        batch_loss = -alpha*(torch.pow((1-probs), self.gamma))*log_p 
-        #print('-----bacth_loss------')
-        #print(batch_loss)
-
-
-        if self.size_average:
-            loss = batch_loss.mean()
-        else:
-            loss = batch_loss.sum()
+        if self.reduction == 'mean':
+            loss = loss.mean()
+        elif self.reduction == 'sum':
+            loss = loss.sum()
         return loss
